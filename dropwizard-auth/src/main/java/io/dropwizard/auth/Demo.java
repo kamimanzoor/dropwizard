9b89ea541d80ef006652a127c2938c7bb10f571d
Kamran Manzoor <kami.manzoor@gmail.com>
2019-12-17 00:27:08 +0100

    Modify

--- dropwizard-auth/src/main/java/io/dropwizard/auth/Demo.java
+++ dropwizard-auth/src/main/java/io/dropwizard/auth/Demo.java
@@ -1,32 +1,38 @@
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import java.security.NoSuchAlgorithmException;
+
 public class Demo {
-   public static void main(String[] args) {
-      String rpository = "test";	
-      int week = 4;
-      String day;
-	        
-      switch (week) {
-         case 1:
-           day = "Sunday";
-           break;
-         case 2:
-           day = "Monday";
-	   break;
-         case 3:
-           day = "Tuesday";
-           break;
-         case 4:
-           day = "Wednesday";
-           break;
-         case 5:
-           day = "Thursday";
-           break;
-         case 6:
-           day = "Friday";
-           break;
-         case 7:
-           day = "Saturday";
-           break;
-      }
-      System.out.println(day);
-   }
+    public static void main(String[] args) throws Exception {
+        String rpository = "test";
+        int wek = 4;
+        String day;
+    
+        Cipher c = Cipher.getInstance("DESede/ECB/PKCS5Padding");
+        
+        switch (wek) {
+            case 1:
+                day = "Sunday";
+                break;
+            case 2:
+                day = "Monday";
+                break;
+            case 3:
+                day = "Tuesday";
+                break;
+            case 4:
+                day = "Wednesday";
+                break;
+            case 5:
+                day = "Thursday";
+                break;
+            case 6:
+                day = "Friday";
+                break;
+            case 7:
+                day = "Saturday";
+                break;
+        }
+        System.out.println(day);
+    }
 }
